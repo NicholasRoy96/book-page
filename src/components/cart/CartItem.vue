73a6ea7cc7e07fe3c7c8592d389764dbe58c7bfe
Nick <nicholas.roy12@gmail.com>
2021-04-16 16:53:49 +0100

    Building / styling cart

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -4,19 +4,24 @@
       <img :src="cartItem.item.image">
     </div>
     <div class="cart-item__main">
-      <h3>
-        {{ cartItem.item.title }}
-      </h3>
-      <h5>
-        {{ author }}
-      </h5>
-      <h5>
-        Quantity: {{ cartItem.qty }}
-      </h5>
-      <h5>
-        {{ price.currency }}
-        {{ price.value }}
-      </h5>
+      <div>
+        <h3 class="cart-item__main__title">
+          {{ cartItem.item.title }}
+        </h3>
+        <h5 class="cart-item__main__author">
+          {{ author }}
+        </h5>
+      </div>
+      <div>
+        <h5 class="cart-item__main__quantity">
+          <strong>Quantity: </strong>
+          {{ cartItem.qty }}
+        </h5>
+        <h5 class="cart-item__main__price">
+          <strong>{{ price.currency }} </strong>
+          {{ price.value }}
+        </h5>
+      </div>
     </div>
     <v-icon
       class="cart-item__remove-button"
@@ -72,13 +77,64 @@ export default {
     }
   }
   &__main {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
     width: 75%;
-    padding-left: var(--spacer-sm);
+    padding: 0 0 var(--spacer-xs) var(--spacer-base);
+    @media (min-width: 768px) {
+      padding: var(--spacer-sm) 0 var(--spacer-sm) var(--spacer-base);
+    }
+    @media (min-width: 1024px) {
+      padding: 0 0 var(--spacer-sm) var(--spacer-base);
+    }
+    &__title {
+      font-family: var(--font-primary);
+      font-size: var(--font-sm);
+      line-height: 1;
+      @media (min-width: 768px) {
+        font-size: var(--font-m);
+        line-height: 1.3;
+      }
+    }
+    &__author {
+      color: var(--c-grey);
+      font-size: var(--font-xs);
+      font-weight: 600;
+    }
+    &__quantity {
+      color: var(--c-black);
+      font-size: var(--font-xs);
+      font-weight: 400;
+      @media (min-width: 768px) {
+        font-size: var(--font-tiny);
+        margin-bottom: var(--spacer-sm);
+      }
+      @media (min-width: 1024px) {
+        margin-bottom: var(--spacer-xxs);
+      }
+      @media (min-width: 1400px) {
+        margin-bottom: var(--spacer-xs);
+      }
+    }
+    &__price {
+      color: var(--c-black);
+      font-size: var(--font-xs);
+      font-weight: 400;
+      @media (min-width: 768px) {
+        font-size: var(--font-tiny);
+      }
+    }
   }
   &__remove-button {
     position: absolute;
-    bottom: var(--spacer-sm);
-    right: var(--spacer-sm);
+    bottom: var(--spacer-xs);
+    right: 0;
+    @media (min-width: 768px) {
+      bottom: var(--spacer-sm);
+      right: var(--spacer-sm);
+    }
   }
 }
 </style>
--- src/components/cart/CartPanel.vue
+++ src/components/cart/CartPanel.vue
@@ -2,13 +2,28 @@
   <div>
     <transition name="width-fade">
       <div v-show="cartOpen" class="cart-panel">
-        <template v-if="cartOpen">
-          <div class="cart-panel__content">
+        <transition name="slow-fade">
+          <div v-if="cartOpen" class="cart-panel__content">
+            <v-btn
+              @click="toggleCartOpen"
+              class="cart-panel__content__close-button"
+              fab
+              dark
+              small
+              color="grey darken-3"
+            >
+              <v-icon dark>
+                mdi-close
+              </v-icon>
+            </v-btn>
             <template v-if="totalCartItems">
               <div class="cart-panel__content__top">
-                <span>My Cart</span>
-                <span>Total items: {{ totalCartItems }}</span>
-                <button @click="toggleCartOpen">Close</button>
+                <span class="cart-panel__content__top__title">
+                  My Cart
+                </span>
+                <span class="cart-panel__content__top__items">
+                  Total items: {{ totalCartItems }}
+                </span>
               </div>
               <section class="cart-panel__content__main">
                 <CartItem
@@ -19,19 +34,35 @@
               </section>
               <div class="cart-panel__content__bottom">
                 <CartPrices />
-                <button class="cart-panel__content__bottom__checkout-button">
+                <button class="cart-panel__cart-button">
                   Secure Checkout
-                  <v-icon small class="cart-panel__content__bottom__checkout-button__icon">
+                  <v-icon small class="cart-panel__cart-button__icon">
                     mdi-lock
                   </v-icon>
                 </button>
               </div>
             </template>
-            <template v-else>
-              There are currently no items in you cart.
-            </template>
+            <transition name="slow-fade">
+              <div v-if="!totalCartItems" class="cart-panel__content__empty-cart">
+                <v-icon x-large color="black">
+                  mdi-shopping-outline
+                </v-icon>
+                <h2 class="cart-panel__content__empty-cart__title">
+                  Your shopping bag is empty...
+                </h2>
+                <h5 class="cart-panel__content__empty-cart__subtitle">
+                  It looks like you haven’t added any items to your bag yet. Start shopping to fill it up.
+                </h5>
+                <button class="cart-panel__cart-button" @click="toggleCartOpen">
+                  Shop Now
+                  <v-icon small class="cart-panel__cart-button__icon">
+                    mdi-cart-outline
+                  </v-icon>
+                </button>
+              </div>
+            </transition>
           </div>
-        </template>
+        </transition>
       </div>
     </transition>
     <transition name="fade">
@@ -76,7 +107,6 @@ export default {
   position: fixed;
   left: 0;
   top: 0;
-  transition: all 0.5s;
   width: 100%;
   height: 100vh;
   opacity: 1;
@@ -85,14 +115,35 @@ export default {
   text-align: start;
   box-shadow: 8px 5px 5px -7px rgba(0,0,0,0.32);
   @media (min-width: 1024px) {
+    width: 45vw;
+  }
+  @media (min-width: 1400px) {
     width: 30vw;
   }
   &__content {
     position: relative;
     width: 100%;
     height: 100%;
+    &__close-button {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      @media (min-width: 1024px) {
+        right: -20px;
+      }
+    }
     &__top {
       padding: var(--spacer-lg);
+      &__title {
+        font-size: var(--font-m);
+        font-weight: bold;
+        margin-right: var(--spacer-xs);
+      }
+      &__items {
+        font-size: var(--font-tiny);
+        font-weight: 500;
+        color: var(--c-offblack);
+      }
     }
     &__main {
       padding: 0 var(--spacer-lg);
@@ -136,6 +187,24 @@ export default {
         }
       }
     }
+    &__empty-cart {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      align-items: center;
+      height: 100%;
+      padding: 0 var(--spacer-lg);
+      &__title {
+        font-family: var(--font-primary);
+        margin: var(--spacer-sm) 0;
+      }
+      &__subtitle {
+        font-size: var(--font-tiny);
+        font-weight: 400;
+        margin-bottom: var(--spacer-sm);
+      }
+    }
   }
   &__overlay {
     background: rgb(33, 33, 33);
@@ -148,6 +217,34 @@ export default {
     right: 0;
     bottom: 0;
   }
+  &__cart-button {
+    width: 100%;
+    margin-top: var(--spacer-base);
+    background: var(--c-blue);
+    color: var(--c-white);
+    border-radius: 4px;
+    padding: var(--spacer-sm) var(--spacer-2xl);
+    font-size: var(--font-sm);
+    font-weight: 600;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transition: all 250ms ease-in-out;
+    border: 2px solid transparent;
+    &:hover{
+      border: 2px solid var(--c-blue);
+      background: var(--c-white);
+      color: var(--c-blue);
+      box-shadow: none;
+    }
+    &:hover &__icon {
+      color: var(--c-blue);
+    }
+    &__icon {
+      color: var(--c-white);
+      transition: all 250ms ease-in-out;
+      margin-left: var(--spacer-xxs);
+      margin-bottom: 2px;
+    }
+  }
 }
 
 // Transition effects
@@ -155,6 +252,12 @@ export default {
   transition: opacity .5s;
 }
 .fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.slow-fade-enter-active, .slow-fade-leave-active {
+  transition: opacity 1.5s;
+}
+.slow-fade-enter, .slow-fade-leave-to {
   opacity: 0;
 }
 .width-fade-enter-active, .width-fade-leave-active {
